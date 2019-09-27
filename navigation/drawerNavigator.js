9e069deb7536e2dc67e8abee157d388358ebe095
gchenu <gchenu@cliniques.nc>
2019-09-27 14:30:21 +1100

    menu drawer

--- navigation/drawerNavigator.js
+++ navigation/drawerNavigator.js
@@ -1,11 +1,12 @@
 import { createAppContainer } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 // SCREENS
 import HomeScreen from '../screens/homeScreen';
 import LoginScreen from '../screens/loginScreen';
 
-const DrawerNavigator = createDrawerNavigator(
+const DrawerNavigator = createBottomTabNavigator(
     {
         Home: HomeScreen,
         Login: LoginScreen
