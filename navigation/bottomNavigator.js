8ec722797c013437d3ed2a4d90a97b795a585ca5
Tran Duc Huy Dimitri <d.duchuytran@cci-formation.nc>
2019-11-14 15:47:57 +1100

    Add homepage and bottom navigation

--- navigation/bottomNavigator.js
+++ navigation/bottomNavigator.js
@@ -1,17 +1,64 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Ionicons from 'react-native-vector-icons/AntDesign';
 
 // SCREENS
 import HomeScreen from '../screens/homeScreen';
 import ProfileScreen from '../screens/profileScreen';
+import LoginScreen from '../screens/loginScreen';
 
 const BottomNavigator = createBottomTabNavigator(
     {
-        profile: ProfileScreen,
         Home: HomeScreen,
+        Profile: ProfileScreen,
+        Login: LoginScreen
     },
     {
-        headerMode: 'screen'
+        headerMode: 'screen',
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+      
+              const { routeName } = navigation.state;
+              let IconComponent = Ionicons;
+              let iconHome;
+              let iconProfile;
+              let iconLogin;
+      
+              if ( routeName === 'Home' ) {
+                iconHome = `bank`;
+                iconProfile = `bank`;
+                iconLogin = `bank`;
+              } else if ( routeName === 'Profile' ) {
+                iconHome = `aliwangwang-o1`;
+                iconProfile = `aliwangwang-o1`;
+                iconLogin = `aliwangwang-o1`;
+             
+              } else if ( routeName === 'Login' ) {
+                iconHome = `API`;
+                iconLogin = `API`;
+                iconLogin = `API`;
+              }
+      
+              return (
+                <IconComponent name={iconHome} size={25} color={tintColor} />,
+                <IconComponent name={iconProfile} size={25} color={tintColor} />,
+                <IconComponent name={iconLogin} size={25} color={tintColor} />
+              );
+            },
+        }),
+
+        tabBarOptions: {
+            activeTintColor: '#1aa3ff',
+            inactiveTintColor: 'gray',
+            activeBackgroundColor:'pink',
+            tabStyle: {
+              borderWidth: 0.5,
+              paddingTop: 6,
+            },
+          },
+
+
     }
 );
 
--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableHighlight, ImageBackground } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 // COMPONENTS
@@ -29,9 +29,18 @@ export class HomeScreen extends React.Component {
         return(
             <View>
                 <CustomHeader navigation={ this.props.navigation } title="Home" />
-                <Text>
-                    Home Screen
-                </Text>
+                <ImageBackground
+                    source={require('../image/home.jpg')}
+                    style={{ width: '100%', height: '100%'}}>
+                    <Text style={styles.title}>Et vous quelle est votre prochaine destination ?</Text>
+                    <Text style={styles.text}>Le covoiturage Calédonien c'est maintenant : Choisissez le trajet qui vous convient le mieux.</Text>
+                    <TouchableHighlight style={styles.btnProp}>
+                        <Text style={styles.btnTextProp}>Proposer un trajet</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.btnSearch}>
+                        <Text style={styles.btnTextSearch}>Rechercher un trajet</Text>
+                    </TouchableHighlight>
+                </ImageBackground>
             </View>
         )
     }
@@ -51,6 +60,66 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+
+    title: {
+        marginTop: 300,
+        marginBottom: 10,
+        fontSize: 22,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#fff',
+        textShadowColor: '#000',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 1
+    },
+    
+    text: {
+        marginBottom: 20,
+        fontSize: 16,
+        textAlign: 'center',
+        color: '#fff',
+        textShadowColor: '#000',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 1
+    },
+    
+    btnProp: {
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        padding: 10,
+        marginRight: 20,
+        marginBottom: 15,
+        marginLeft: 20,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#000',
+        backgroundColor: '#1aa3ff',
+    },
+    
+    btnSearch: {
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        padding: 10,
+        marginRight: 20,
+        marginBottom: 15,
+        marginLeft: 20,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#000',
+        backgroundColor: '#fff',
+    },
+    
+    btnTextProp: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    
+    btnTextSearch: {
+        fontSize: 16,
+        color: '#000',
+        fontWeight: 'bold',
     },
   });
   
