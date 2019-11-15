6439d62f9d7210ae83558539c8be64b8b5a80a05
gchenu <gchenu@cliniques.nc>
2019-11-15 11:19:58 +1100

    Update registerScreen.js

--- screens/registerScreen.js
+++ screens/registerScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -38,54 +38,57 @@ export class RegisterScreen extends React.Component {
 
     render() {
         return(
-            <View>
+            <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" enabled>
                 <CustomHeader navigation={ this.props.navigation } title="Register" />
-                <Input
-                        onChangeText={ (username) => this.setState({username}) }
-                        inputContainerStyle={styles.input}
-                        placeholder='Nom de compte'
-                        rightIcon={
-                            <Icon name='user' size={32} color='#2089DC' />
-                        }
-                    />
-                    <Input
-                        onChangeText={ (email) => this.setState({email}) }
-                        inputContainerStyle={styles.input}
-                        placeholder='Adresse Mail'
-                        rightIcon={
-                            <Icon name='envelope' size={24} color='#2089DC' />
-                        }
-                    />
-                                        <Input
-                        onChangeText={ (phone) => this.setState({phone}) }
-                        inputContainerStyle={styles.input}
-                        placeholder='Numéro de téléphone'
-                        rightIcon={
-                            <Icon name='phone' size={24} color='#2089DC' />
-                        }
-                    />
-                    <Input
-                        onChangeText={ (password) => this.setState({password}) }
-                        inputContainerStyle={styles.input}
-                        placeholder='Mot de passe'
-                        secureTextEntry={true}
-                        rightIcon={
-                            <Icon name='lock' size={32} color='#2089DC' />
-                        }
-                    />
-                                        <Input
-                        onChangeText={ (cpassword) => this.setState({cpassword}) }
-                        inputContainerStyle={styles.input}
-                        placeholder='Confirmer le mot de passe'
-                        secureTextEntry={true}
-                        rightIcon={
-                            <Icon name='lock' size={32} color='#2089DC' />
-                        }
-                    />
-                    <View style={styles.buttons}>
-                        <Button title="S'inscrire" onPress={this.handleSubmit} />
-                    </View>
-            </View>
+                <View style={styles.container}>
+                        <Input
+                            onChangeText={ (username) => this.setState({username}) }
+                            inputContainerStyle={styles.input}
+                            placeholder='Nom de compte'
+                            rightIcon={
+                                <Icon name='user' size={32} color='#2089DC' />
+                            }
+                        />
+                        <Input
+                            onChangeText={ (email) => this.setState({email}) }
+                            inputContainerStyle={styles.input}
+                            placeholder='Adresse Mail'
+                            rightIcon={
+                                <Icon name='envelope' size={24} color='#2089DC' />
+                            }
+                        />
+                                            <Input
+                            onChangeText={ (phone) => this.setState({phone}) }
+                            inputContainerStyle={styles.input}
+                            placeholder='Numéro de téléphone'
+                            rightIcon={
+                                <Icon name='phone' size={24} color='#2089DC' />
+                            }
+                        />
+                        <Input
+                            onChangeText={ (password) => this.setState({password}) }
+                            inputContainerStyle={styles.input}
+                            placeholder='Mot de passe'
+                            secureTextEntry={true}
+                            rightIcon={
+                                <Icon name='lock' size={32} color='#2089DC' />
+                            }
+                        />
+                                            <Input
+                            onChangeText={ (cpassword) => this.setState({cpassword}) }
+                            inputContainerStyle={styles.input}
+                            placeholder='Confirmer le mot de passe'
+                            secureTextEntry={true}
+                            rightIcon={
+                                <Icon name='lock' size={32} color='#2089DC' />
+                            }
+                        />
+                        </View>
+                        <View style={styles.buttons}>
+                            <Button title="S'inscrire" onPress={this.handleSubmit} />
+                        </View>
+                    
+            </KeyboardAvoidingView>
         )
     }
 }
@@ -104,10 +107,20 @@ export default connect(mapStateToProps, mapDispatchToProps)(RegisterScreen);
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginEnd: 35,
+        marginStart: 35
+    },
+    input: {
+        borderColor:'#2089DC',
+        marginTop: 20
+    },
+    buttons: {
+        padding: 10,
+        marginEnd: 35,
+        marginStart: 35
     },
   });
   
