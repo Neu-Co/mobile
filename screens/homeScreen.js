453347b4671b0b40f867d34e20776fd515147328
gchenu <gchenu@cliniques.nc>
2020-03-06 09:44:32 +1100

    Update homeScreen.js

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text, ImageBackground } from 'react-native';
+import { Button, Image, Avatar } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // COMPONENTS
 import CustomHeader from '../components/header';
@@ -27,21 +29,49 @@ export class HomeScreen extends React.Component {
 
     render() {
         return(
-            <View>
-                <CustomHeader navigation={ this.props.navigation } title="Home" />
-                <ImageBackground
-                    source={require('../image/home.jpg')}
-                    style={{ width: '100%', height: '100%'}}>
+            <ImageBackground
+                source={require('../image/home.jpg')}
+                style={styles.image}>
+                <View style={styles.container}>
+
+                    <View style={styles.logoContainer}>
+                        <Avatar
+                            rounded 
+                            source={require('../assets/logo.png')}
+                            style={{width: 150, height: 150, borderWidth: 2, borderRadius: 18, borderColor: '#1aa3ff'  }}
+                        />
+                    </View>
+
                     <Text style={styles.title}>Et vous quelle est votre prochaine destination ?</Text>
                     <Text style={styles.text}>Le covoiturage Calédonien c'est maintenant : Choisissez le trajet qui vous convient le mieux.</Text>
-                    <TouchableOpacity style={styles.btnProp}>
-                        <Text style={styles.btnTextProp}>Proposer un trajet</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btnSearch}>
-                        <Text style={styles.btnTextSearch}>Rechercher un trajet</Text>
-                    </TouchableOpacity>
-                </ImageBackground>
-            </View>
+
+                    <View style={styles.btnGroup}>
+                        <Button
+                            title='Proposer un trajet'
+                            buttonStyle={styles.btn}
+                            icon={{
+                                name: 'add',
+                                size: 30,
+                                color: 'white'
+                            }}
+                        />
+                        <Button
+                            title='Rechercher un trajet'
+                            buttonStyle={styles.btn}
+                            icon={{
+                                name: 'search',
+                                size: 30,
+                                color: 'white'
+                            }}
+                            onPress={() => {
+                                this.props.navigation.navigate('Annonces');
+                            }}
+                        />
+                    </View>
+                    
+                    
+                </View>
+            </ImageBackground>
         )
     }
 }
@@ -55,59 +85,39 @@ const mapStateToProps = (state) => {
 export default connect(mapStateToProps)(HomeScreen);
 
 const styles = StyleSheet.create({
+    
+    image: {
+        height: '100%',
+        width: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        justifyContent: 'center'
     },
 
     title: {
-        marginTop: 300,
         marginBottom: 10,
         fontSize: 22,
         fontWeight: 'bold',
         textAlign: 'center',
-        color: '#fff',
-        textShadowColor: '#000',
-        textShadowOffset: {width: -1, height: 1},
-        textShadowRadius: 1
+        color: '#000'
     },
     
     text: {
         marginBottom: 20,
         fontSize: 16,
         textAlign: 'center',
-        color: '#fff',
-        textShadowColor: '#000',
-        textShadowOffset: {width: -1, height: 1},
-        textShadowRadius: 1
+        color: '#000'
     },
     
-    btnProp: {
-        alignSelf: 'stretch',
-        alignItems: 'center',
+    btn: {
         padding: 10,
-        marginRight: 20,
-        marginBottom: 15,
-        marginLeft: 20,
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: '#000',
+        margin: 10,
         backgroundColor: '#1aa3ff',
-    },
-    
-    btnSearch: {
-        alignSelf: 'stretch',
-        alignItems: 'center',
-        padding: 10,
-        marginRight: 20,
-        marginBottom: 15,
-        marginLeft: 20,
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: '#000',
-        backgroundColor: '#fff',
     },
     
     btnTextProp: {
@@ -120,6 +130,14 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#000',
         fontWeight: 'bold',
+    },
+
+    logoContainer: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+        borderRadius: 50
     },
   });
   
