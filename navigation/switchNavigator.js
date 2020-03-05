d97c5d7ab34eebb11db5e0a05f010405cb1cd119
gchenu <gchenu@cliniques.nc>
2020-03-06 08:26:06 +1100

    setup authstack

--- navigation/switchNavigator.js
+++ navigation/switchNavigator.js
@@ -1,24 +1,7 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-
-// Screens
-import LoginScreen from '../screens/loginScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
-import RegisterScreen from '../screens/registerScreen';
-import profileScreen from '../screens/profileScreen';
-
-// Navigator
 import BottomNavigator from './bottomNavigator';
-
-
-const AuthStack = createStackNavigator(
-    { 
-        login: LoginScreen,
-        register: RegisterScreen
-    },
-    {
-        headerMode: 'none'
-    });
+import AuthStack from './authstackNavigator';
 
 const SwitchNavigator = createSwitchNavigator(
     {
